d4f4d297959fcd5335d743565aadbc75f7c30f4c
marclecha05 <marclecha05@gmail.com>
2025-03-18 09:47:45 +0100

    18/03/2025 inicio

---
+++ src/main/java/com/example/proyectofinalgs/ProyectoFinalGsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectofinalgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalGsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalGsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectofinalgs/ProyectoFinalGsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectofinalgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalGsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
