a5bdf4e76a77e7f409c3f696670ade1df7ec881a
marclecha05 <marclecha05@gmail.com>
2025-05-05 16:05:25 +0200

    5/5/2025 juan al rescate

--- src/main/java/com/example/proyectofinalgs/Controllers/LoginController.java
+++ src/main/java/com/example/proyectofinalgs/Controllers/LoginController.java
@@ -1,11 +1,13 @@
 package com.example.proyectofinalgs.Controllers;
 
 import com.example.proyectofinalgs.Services.UserService;
+import jakarta.servlet.annotation.WebServlet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@WebServlet
 public class LoginController {
 
     private final UserService userService;
